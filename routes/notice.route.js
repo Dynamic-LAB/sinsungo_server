93ad19c405d75f01a8bd6a275c706f4c27da5d4c
djelalswns12 <djelalswns12@naver.com>
2021-06-03 20:21:54 +0900

    병합

--- routes/notice.route.js
+++ routes/notice.route.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const noticeController = require('../controllers/notice.controller');
 
 router.get('/', noticeController.findAll);
-router.get('/:id', noticeController.findOne);
+router.get('/:id', noticeController.findOne); 
 module.exports = router;
